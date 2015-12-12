e31fc6f8c557617a8035c7fd3870fd22de8f8151
arasselvi <aras.selvi@ozu.edu.tr>
2015-12-12 23:39:07 +0200

    Minor Fix

--- CS320LibraryDBM/CS320LDBMS/CS320LDBMS/AdminOption.cpp
+++ CS320LibraryDBM/CS320LDBMS/CS320LDBMS/AdminOption.cpp
@@ -28,11 +28,11 @@ void AdminOption::loanSetter(Book c)
 void AdminOption::changeBookInfo(Book book)
 {
 	int a;
-	string oldname = book.getName;
-	string olauthor = book.getAuthor;
-	string oldid = book.getID;
-	string oldyear = book.getYear;
-	cout << "What wrong information would you like to change in the book named: " << book.getName << endl;
+	string oldname = book.getName();
+	string olauthor = book.getAuthor();
+	string oldid = book.getID();
+	string oldyear = book.getYear();
+	cout << "What wrong information would you like to change in the book named: " << book.getName() << endl;
 	cout << "press 1 for name" << endl;
 	cout << "press 2 for author" << endl;
 	cout << "press 3 for id" << endl;
@@ -52,21 +52,21 @@ void AdminOption::changeBookInfo(Book book)
 		string newauthor;
 		cin >> newauthor;
 		book.setAuthor(newauthor);
-		cout << "New author for the book named " << book.getName << " is set to be " << newauthor;
+		cout << "New author for the book named " << book.getName() << " is set to be " << newauthor;
 	}
 	else if (a == 3) {
 		cout << "New id: " << endl;
 		string newid;
 		cin >> newid;
 		book.setID(newid);
-		cout << "New author for the book named " << book.getName << " is set to be " << newid;
+		cout << "New author for the book named " << book.getName() << " is set to be " << newid;
 	}
 	else if (a == 4 ){
 		cout << "New year: " << endl;
 		string newyear;
 		cin >> newyear;
 		book.setYear(newyear);
-		cout << "New author for the book named " << book.getName << " is set to be " << newyear;
+		cout << "New author for the book named " << book.getName() << " is set to be " << newyear;
 	}
 	else {
 		cout << "Wrong input noob!" << ends;
@@ -77,11 +77,11 @@ void AdminOption::changeBookInfo(Book book)
 void AdminOption::changeStudentInfo(Student student)
 {
 	int a;
-	string oldname = student.getName;
-	string oldsurname = student.getSurname;
-	string oldid = student.getID;
-	string oldemail = student.getEmail;
-	cout << "What wrong information would you like to change in the student named: " << student.getName << endl;
+	string oldname = student.getName();
+	string oldsurname = student.getSurname();
+	string oldid = student.getID();
+	string oldemail = student.getEmail();
+	cout << "What wrong information would you like to change in the student named: " << student.getName() << endl;
 	cout << "press 1 for name" << endl;
 	cout << "press 2 for surname" << endl;
 	cout << "press 3 for ID" << endl;
@@ -101,21 +101,21 @@ void AdminOption::changeStudentInfo(Student student)
 		string newsurname;
 		cin >> newsurname;
 		student.setSurname(newsurname);
-		cout << "New surname for the student named " << student.getName << " is set to be " << newsurname;
+		cout << "New surname for the student named " << student.getName() << " is set to be " << newsurname;
 	}
 	else if (a == 3) {
 		cout << "New id: " << endl;
 		string newid;
 		cin >> newid;
 		student.setID(newid);
-		cout << "New id for the student named " << student.getName << " is set to be " << newid;
+		cout << "New id for the student named " << student.getName() << " is set to be " << newid;
 	}
 	else if (a == 4) {
 		cout << "New emai: " << endl;
 		string newemail;
 		cin >> newemail;
 		student.setEmail(newemail);
-		cout << "New email for the student named " << student.getName << " is set to be " << newemail;
+		cout << "New email for the student named " << student.getName() << " is set to be " << newemail;
 	}
 	else {
 		cout << "Wrong input noob!" << ends;
