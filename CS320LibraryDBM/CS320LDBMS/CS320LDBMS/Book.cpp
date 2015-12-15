38ce802e2fcfcca0177f194c6080ea03cf7a3564
daniyal2010 <daniyal.anwar@ozu.edu.tr>
2015-12-15 11:47:59 +0200

    Added borrow and return functionality
    still buggy and needs improvement

--- CS320LibraryDBM/CS320LDBMS/CS320LDBMS/Book.cpp
+++ CS320LibraryDBM/CS320LDBMS/CS320LDBMS/Book.cpp
@@ -4,12 +4,81 @@ Book::Book()
 {
 }
 
+Book::Book(string name, string ISBN, string year) {
+	this->name = name;
+	this->ISBN = ISBN;
+	this->year = year;
+
+}
 Book::~Book()
 {
 }
 
+
 void Book::bookMenu() {
 
-	// in this function you will create a menu to help the user borrow a book or return a book.
+
+	cout << "welcome to the book Section" << endl;
+	cout << "For Book information press 1" << endl;
+	cout << "For borrowing the book press 2" << endl;
+	cout << "For returning the book press 3" << endl;
+
+
+	int option;
+	cout << "Choose an option" << endl;
+	cin >> option;
+
+	if (option == 1) {
+		BookInformation();
+	}
+	else
+		if (option == 2) {
+			BorrowBook();
+		}
+		else
+			if (option == 3) {
+				returnBook();
+			}
+}
+bool Book::BookInformation() {
+	string name;
+	string ISBN;
+	string year;
+
+
+	cout << "Please enter the Book Information" << endl;
+	cout << "Please enter the book name: " << endl;
+	cin >> name;
+	cout << "please enter the book ISBN : " << endl;
+	cin >> ISBN;
+	cout << "please Enter the book year: " << endl;
+	cin >> year;
+
+	checkAvailability();
+
+	return false;
+
+
+
+
 
 }
+void Book::BorrowBook() {
+
+	bool available = true;
+	string name;
+	cout << "please enter the Required informaton: " << endl;
+	cout << "Enter the Book Name: " << endl;
+	cin >> name;
+
+
+}
+
+void Book::returnBook() {
+
+}
+
+void Book::checkAvailability() {
+
+}
+
--- CS320LibraryDBM/CS320LDBMS/CS320LDBMS/Book.h
+++ CS320LibraryDBM/CS320LDBMS/CS320LDBMS/Book.h
@@ -3,17 +3,26 @@
 
 #include<iostream>
 #include<string>
+#include <fstream>
 
 using namespace std;
 
 class Book {
 public:
 	Book();
+	Book(string, string, string);
 	~Book();
-	
+	bool BookInformation();
+	void BorrowBook();
+	void checkAvailability();
 	void bookMenu();
+	void returnBook();
 
 private:
-	
+
+	string name;
+	string ISBN;
+	string year;
+
 };
 #endif
